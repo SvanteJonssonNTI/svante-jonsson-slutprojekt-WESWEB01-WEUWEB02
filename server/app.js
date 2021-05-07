d781d166c2b19cd8be463926e0c81d49261658b1
Svante-J-TE <svante43@telia.com>
2021-05-07 13:59:51 +0200

    worked on navbar design
    the navbar design is almost finished, however there is a few things that have to be sorted out

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ app.set("view engine", "ejs");
 
 //USE
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/client/"));
 
 //GET
 app.get("/", loginRegister.checkAuthenticated, async (req, res) => {
