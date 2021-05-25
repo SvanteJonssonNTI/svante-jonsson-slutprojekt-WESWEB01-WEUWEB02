921357db89af38329b79c6bd7c395b3239b3dd5a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-05-25 18:53:19 +0200

    finished cart design

--- server/client/js/popover.js
+++ server/client/js/popover.js
@@ -1,8 +1,12 @@
 
-var popoverBtn = document.getElementById("btn-action");
+var popoverBtn = document.getElementById("btn-popover");
 var popover = new bootstrap.Popover(popoverBtn, {
   placement: "bottom",
   html: true,
   container: 'body',
   content: document.getElementById("popover-content").innerHTML
 });
+
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+  trigger: 'focus'
+})
