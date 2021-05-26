58a76f3874ebd4b52d902aa6f93536489622af17
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-05-26 21:46:28 +0200

    created function that populates the database with products

--- server/populateDatabase.js
+++ server/populateDatabase.js
@@ -1,3 +1,19 @@
 const product = require("./product");
+const db = require("./dbModule");
+const adjectives = require("./adjectives")
 
-//addProduct.createProduct()
+console.log()
+
+db.connectToMongoose("webshop")
+
+productCreate();
+
+
+async function productCreate() {
+    for(let i = 0; i < 20; i++){
+        await db.saveToMongoose(
+            product.createProduct((adjectives[Math.floor(Math.random() * adjectives.length)] + " Ljus"), "lite beskrivnings text", "nedladdning.jpg", Math.floor(Math.random() * 2000), Math.floor(Math.random() * 2000))
+            )
+    }
+    
+}
